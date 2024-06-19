a6c28a6ceff4aa06bae7043a8bcb02e9f9e4105f
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2024-06-19 21:00:11 +0200

    added the word check for potentially offensive content in images etc. gore, adult, medical etc.

--- Moderator.py
+++ Moderator.py
@@ -1,14 +1,19 @@
+import os
+
 from flask import Flask, request, jsonify
+from google.cloud import vision
 from transformers import pipeline
 
 app = Flask(__name__)
 
 classifier = pipeline('sentiment-analysis')
 
-#Here you add words you don't want to be contained
-offensive_keywords = [
-    "stupid"
-]
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcp_key.json"
+vision_client = vision.ImageAnnotatorClient()
+
+offensive_keywords = ["stupid"]
+
+
 def contains_offensive_keywords(text):
     for word in offensive_keywords:
         if word.lower() in text.lower():
@@ -22,6 +27,29 @@ def check_text():
     text = data['text']
 
     if contains_offensive_keywords(text):
+        return jsonify({'isAllowed': False})
+
+    return jsonify({'isAllowed': True})
+
+
+@app.route('/check-image', methods=['POST'])
+def check_image():
+    file = request.files['image']
+    content = file.read()
+
+    image = vision.Image(content=content)
+    response = vision_client.safe_search_detection(image=image)
+    safe = response.safe_search_annotation
+
+    print('Safe search results:')
+    print(f'Adult: {safe.adult}')
+    print(f'Spoof: {safe.spoof}')
+    print(f'Medical: {safe.medical}')
+    print(f'Violence: {safe.violence}')
+    print(f'Racy: {safe.racy}')
+
+    if safe.adult >= vision.Likelihood.LIKELY or safe.violence >= vision.Likelihood.LIKELY or safe.racy >= vision.Likelihood.LIKELY:
+        print('The image contains inappropriate content.')
         return jsonify({'isAllowed': False})
 
     return jsonify({'isAllowed': True})
